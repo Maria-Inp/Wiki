c4e9ad47759839137f6e5f2cc3fc502018474356
Maria-Inp <maryam.irannezhad1224@gmail.com>
2022-10-18 20:13:13 +0330

    Create urls.py

---
+++ encyclopedia/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+app_name = 'encyclopedia'
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("newEntry", views.newEntry, name="newEntry"),
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
+    path("random", views.randomPage, name="randomPage")
+]
